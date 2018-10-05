6a6883061a9bebc66aa3ec9ab5fa605105846c08
Artem Ivanov <fast0490f@mail.ru>
2018-10-05 13:07:36 +0300

    test

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const Plugin = require('./lib/plugin');
 
 const plugin = new Plugin();
 
+var Peer = require('simple-peer')
+var wrtc = require('wrtc')
+
+var peer1 = new Peer({ initiator: true, wrtc: wrtc })
+var peer2 = new Peer({ wrtc: wrtc })
+
 
 plugin.on('params', params => {
   start(params);
@@ -17,5 +23,26 @@ plugin.on('debug', mode => {});
 function start(options) {
   plugin.debug("version: 0.0.5");
   plugin.debug("start");
-  require('./test.js')
+  test()
+}
+
+function test() {
+
+  peer1.on('signal', function (data) {
+    peer2.signal(data)
+  })
+
+  peer2.on('signal', function (data) {
+    peer1.signal(data)
+  })
+
+  peer1.on('connect', function () {
+    plugin.debug('send message');
+    peer1.send('data reciverd!')
+  })
+
+  peer2.on('data', function (data) {
+    plugin.debug(data.toString());
+  })
+
 }
